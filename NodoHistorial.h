e286dd694e3f1bf6bf2a5f115fa474a9bda89939
Steve Eduardo Montoya Madrigal <steve.montoya.madrigal@est.una.ac.cr>
2026-05-13 15:29:26 -0600

    se crea NodoHistorial.h

---
+++ NodoHistorial.h
@@ -0,0 +1,28 @@
+#pragma once
+#ifndef Nodo_HISTORIAL_H
+#define NODO_HISTORIAL_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+namespace EIF201 {
+
+	struct NodoHistorial {
+		string  cancion;
+		NodoHistorial* siguiente;
+		NodoHistorial* anterior;
+
+		NodoHistorial(const string& c)
+			: cancion(c), siguiente(nullptr),
+			anterior(nullptr) {
+			cout << "[NodoHistorial creado: "
+				<< c << "]" << endl;
+		}
+		NodoHistorial() {
+			cout << "[NodoHistorial destruido: " << cancion << "]" << endl;
+		}
+		
+	};
+}
+#endif
